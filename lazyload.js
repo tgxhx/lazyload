edf06a9afbfd21377c2ac28c5781ac011bf4902d
tgxh2020 <tgxh2020@gmail.com>
2017-06-24 18:10:59 +0800

    ...

--- lazyload.js
+++ lazyload.js
@@ -7,8 +7,9 @@
       delay: 500, //延迟
       time: 1000  //滚动间隔
     },
-    n = 0   //从第n个开始
+    n = 0   //从第n个开始，防止滚动到一定距离时又从第一张图片开始设置src
 
+  //判断类型
   function getType(obj) {
     return Object.prototype.toString.call(obj).match(/^\[object\s(.*)\]$/)[1]
   }
@@ -16,8 +17,9 @@
   function lazyload() {
     var clientHeight = document.documentElement.clientHeight,
       scrollTop = document.body.scrollTop
-    
+
     for (var i = n; i < imgs.length; i++) {
+      //判断图片距离页面顶部的距离和窗口可见高度加滚动高度的大小
       if (imgs[i].offsetTop < clientHeight + scrollTop) {
         if (imgs[i].getAttribute('src') !== '') {
           imgs[i].src = imgs[i].getAttribute('data-src')
@@ -27,15 +29,18 @@
     }
   }
 
+  //节流函数，限制触发频率
   function throttle(fun, delay, time) {
     var timer,
       startTime = new Date()
 
+    //使用闭包设置私有变量
     return function () {
       var context = this,
         args = arguments,
         curTime = new Date()
       clearTimeout(timer)
+      //比较两次滚动时间的间隔是否大于设置的频率
       if (curTime - startTime >= time) {
         fun.call(context, args)
         startTime = curTime
